0abf5b53a2ac58558fd2ff0f2964f441caf58afa
ObiJuanKenobi <venegas@iastate.edu>
2016-11-05 11:27:11 -0500

    Added app files

---
+++ InteractiveStory/app/src/main/java/com/example/junio/interactivestory/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.junio.interactivestory;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mNameField;
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNameField = (EditText)findViewById(R.id.nameEditText);
+        mButton = (Button)findViewById(R.id.Start_Button);
+
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name =  mNameField.getText().toString();
+                startStory(name);
+            }
+        });
+    }
+
+    private void startStory(String s){
+        Intent intent = new Intent(MainActivity.this, StoryActivity.class);
+        intent.putExtra(getString(R.string.key_name), s);
+        startActivity(intent);
+    }
+}
